6503af62ebf1b525d60cdbee417a7b1ba73f5a9a
Henrietta <hetty8004@gmail.com>
2023-03-16 15:52:30 -0700

    my first line of codes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,12 @@
 #include <unistd.h>
 /**
  * main - A C program that prints a new line to a standard error
+ *
  * Return: 1 (successful)
  */
 int main(void)
 {
 	write(2,
-	 "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n" 59);
+	 "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" 59);
 	return (1);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,6 @@
 int main(void)
 {
 	printf("size of a char: %d byte(S)\n", sizeof(char));
-
 	printf("size of an int: %d byte(S)\n", sizeof(int));
 	printf("size of a long int: %d byte(S)\n", sizeof(long int));
 	printf("size of a long long int: %d byte(S)\n", sizeof(long long int));
