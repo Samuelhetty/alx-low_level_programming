9e4c46bafd0e7b154ce68f8fd3d925016fe1a290
Henrietta <hetty8004@gmail.com>
2023-03-16 23:22:09 -0700

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,12 @@
 #include <unistd.h>
 /**
  * main - A C program that prints a new line to a standard error
- *
- * Return: 1 (successful)
+ * Return: 1 (error)
  */
 int main(void)
 {
-	write(2,
-	 "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" 59);
+	char quo[] = "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n";
+	
+	write(1, quo, 59);
 	return (1);
 }
