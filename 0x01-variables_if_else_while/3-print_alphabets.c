94fbe908c25536e74a531d58e4261c3316c8f432
Henrietta <hetty8004@gmail.com>
2023-03-17 15:44:31 -0700

    started to code using c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,13 +8,13 @@
  */
 int main(void)
 {
-        char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
-
-        for (i = 0; i < 52; i++)
+	
+	for (i = 0; i < 52; i++)
 	{
 		putchar(alp[i]);
 	}
-        putchar('\n');
-        return (0);
+	putchar('\n');
+	return (0);
 }
