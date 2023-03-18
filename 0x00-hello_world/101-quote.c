a7e2561476a89986198eb8b931dbd11c86f5f283
Henrietta <hetty8004@gmail.com>
2023-03-18 14:39:01 -0700

    my first line of codes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A C program that prints a new line to a standard error
+ * main - prints a new line to a standard error
  * Return: 1 (error)
  */
 int main(void)
 {
 	write(2,
-	"and that piece of art is useful\" - Dora Korpar, 2015-10-19"\n, 59);
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,11 +6,11 @@
  */
 int main(void)
 {
-	printf("size of a char: %d byte(s)\n", (unsigned long)sizeof(char));
-	printf("size of an int: %d byte(s)\n", (unsigned long)sizeof(int));
-	printf("size of a long int: %d byte(s)\n", (unsigned long)sizeof(long int));
-	printf("size of a long long int: %d byte(s)\n", (unsigned long)sizeof
+	printf("Size of a char: %d byte(s)\n", (unsigned long)sizeof(char));
+	printf("Size of an int: %d byte(s)\n", (unsigned long)sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", (unsigned long)sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", (unsigned long)sizeof
 			(long long int));
-	printf("size of a float: %d byte(s)\n", (unsigned long)sizeof(float));
+	printf("Size of a float: %d byte(s)\n", (unsigned long)sizeof(float));
 	return (0);
 }
