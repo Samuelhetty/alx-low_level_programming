6e9838e016da3316ba7d2a070f14ab197c416549
Henrietta <hetty8004@gmail.com>
2023-03-22 13:06:22 -0700

    errors amended

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -31,7 +31,7 @@ int main(void)
 	for (i = 92; i < 99; ++i)
 	{
 		printf(", %lu", aft1 + (aft2 / 1000000000));
-		pritf("%lu", aft2 % 1000000000);
+		printf("%lu", aft2 % 1000000000);
 		aft1 = aft1 + bef1;
 		bef1 = aft1 - bef1;
 		aft2 = aft2 + bef2;
