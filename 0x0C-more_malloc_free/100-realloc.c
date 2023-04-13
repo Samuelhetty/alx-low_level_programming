fc06ce27ccaa6b72ce8a59453a4a68199f8cd07f
Henrietta <hetty8004@gmail.com>
2023-04-13 13:33:03 -0700

    task 100-realloc.c resolved

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -4,7 +4,7 @@
 
 /**
   * _realloc - reallocates a memory block using malloc and free
-  * @ptr: pointer to the memory previously allocated 
+  * @ptr: pointer to the memory previously allocated
   * @old_size: the size, in bytes, of the allocated space for ptr
   * @new_size: the new size, in bytes of the new memory block
   *
