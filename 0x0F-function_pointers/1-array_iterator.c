df5aa3c03a12ef007a9079c0d453a6166d3dcba2
Henrietta <hetty8004@gmail.com>
2023-04-20 13:00:59 -0700

    100-main_opcodes.c mended

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,10 +12,14 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	size_t i;
 
 	if (array && action)
-		return;
-
-	for (i = 0; i < size; i++)
 	{
-		action(array[i]);
+		i = 0;
+		
+		while (i < size)
+		{
+			action(array[i]);
+			i++;
+		}
+
 	}
 }
---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_opcodes - prints the opcodes
+ * @n: number of bytes
+ * @a: address
+ *
+ * Return: void
+ */
+void print_opcodes(char *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%02hhx\n", a[i]);
+		if (i < n - 1)
+			printf(" ");
+	}
+	printf("\n");
+}
+
+/**
+ * main - prints its own opcodes
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int bytes;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	print_opcodes((char *) &main, bytes);
+	return (0);
+}
