7ee12e6e2295bec2cd5641df3f3b1b3ef0795c73
Henrietta Onoge <hetty8004@gmail.com>
2023-07-19 07:43:09 -0700

    A goal is not always meant to be reached, it often serves simply as something to aim at

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * array_iterator - prints each array element on a newline
+ * @array: array
+ * @size: size of array
+ * @action: pointer to the function you need to use
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array && action)
+	{
+		i = 0;
+
+		while (i < size)
+		{
+			action(array[i]);
+			i++;
+		}
+
+	}
+}
