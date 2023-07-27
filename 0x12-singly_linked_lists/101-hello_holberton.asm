f7127ff58087b8c961144afe0668668ffa25acdf
Henrietta Onoge <hetty8004@gmail.com>
2023-07-27 13:22:45 -0700

    assembly code written

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,15 @@
+       SECTION .data
+msg:   db 'Hello, Holberton\n', 0
+fmt:   db "%s", 10, 0
+
+       SECTION .text
+       extern printf
+       global main
+main:
+       mov   edi, fmt
+       mov   edi, msg
+       mov   eax, 0
+       call  printf
+
+       mov   eax, 0
+       ret
