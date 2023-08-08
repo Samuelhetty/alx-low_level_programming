97b22df8d343cba3d83047ee2c4ee6e3ea08766d
Henrietta <hetty8004@gmail.com>
2023-08-08 07:30:20 +0200

    header files resolved

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,7 +10,7 @@
 #include <unistd.h>
 
 int end_func(int num, char *str);
-size_t _attribute__ ((warn_unused_result)) _strlen(const char *s);
+size_t __attribute__ ((warn_unused_result)) _strlen(const char *s);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
