f309bb35bcbcce673e156d3f6b77e859b059f77b
Henrietta Onoge <hetty8004@gmail.com>
2023-10-12 21:17:20 -0700

    task2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -16,9 +16,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 
 	n_node->n = n;
-	n_node->prev = NULL;
+	
 	n_node->next = (*head);
+	if (*head)
+		(*head)->prev = n_node;
 	(*head) = n_node;
 
-	return (*head);
+	return (n_node);
 }
