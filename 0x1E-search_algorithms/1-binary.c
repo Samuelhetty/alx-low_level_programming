012903eed452e377848586012cd9bd4d326afae5
Henrietta Onoge <hetty8004@gmail.com>
2024-02-06 06:10:45 -0800

    search algorithms

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,57 @@
+#include "search_algos.h"
+/**
+ * print_array - prints the input array
+ * @array: the input array
+ * @min: the minimum index number to print
+ * @max: the maximum index number to print
+ */
+void print_array(int *array, size_t min, size_t max)
+{
+	printf("Searching in array: ");
+	while (min <= max)
+	{
+		printf("%d", array[min]);
+		printf("%s", min++ < max ? ", " : "\n");
+	}
+}
+/**
+ * arr_bs - searches input array recursively for input value using 'binary search'
+ * @array: the input array
+ * @min: the minimum index of the array
+ * @max: the maximum index of the array
+ * @value: the value to search for
+ *
+ * Return: the index of where the found value is or -1 on error
+ */
+int arr_bs(int *array, size_t min, size_t max, int value)
+{
+	size_t mid;
+
+	print_array(array, min, max);
+	mid = min + ((max - min) / 2);
+	if (value == array[mid])
+		return (mid);
+	if (min == max || max == 0)
+		return (-1);
+
+	if (value > array[mid])
+		min = mid + 1;
+	else
+		max = mid;
+	return (arr_bs(array, min, max, value));
+}
+/**
+ * binary_search - searches input array for input value using 'linear search'
+ * @array: the input array
+ * @size: the size of array
+ * @value: the value to search for
+ *
+ * Return: the index of where the found value is or -1 on error
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	if (array && size)
+		return (arr_bs(array, 0, size - 1, value));
+	else
+		return (-1);
+}
---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,57 @@
+#include "search_algos.h"
+#include <math.h>
+/**
+ * arr_ls - searches input array for input value using 'linear search'
+ * @array: the input array
+ * @min: the minimum index value to check
+ * @max: the maximum index value to check
+ * @value: the value to search for
+ *
+ * Return: the index of where the found value is or -1 on error
+ */
+int arr_ls(int *array, size_t min, size_t max, int value)
+{
+	while (min <= max)
+	{
+		printf("Value checked array[%lu] = [%d]\n", min,
+		       array[min]);
+		if (array[min] == value)
+			return (min);
+		min++;
+	}
+	return (-1);
+}
+/**
+ * jump_search - searches input array for input value using 'jump search'
+ * @array: the input array
+ * @size: the size of array
+ * @value: the value to search for
+ *
+ * Return: the index of where the found value is or -1 on error
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	size_t i, prev, step, range;
+
+	if (!array || !size)
+		return (-1);
+	range = (size_t)sqrt(size);
+	step = prev = 0;
+	while (1 == 1)
+	{
+		i = step * range;
+		if (i >= size || array[i] >= value)
+		{
+			printf("Value found between indexes");
+			printf(" [%lu] and [%lu]\n", prev, i);
+			if (i >= size)
+				i = size - 1;
+			return (arr_ls(array, prev, i, value));
+		}
+
+		printf("Value checked array[%lu] = [%d]\n",
+		       i, array[i]);
+		prev = i;
+		step++;
+	}
+}
